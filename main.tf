6f428652ca06fa51e22b38433da8fad765fa70e7
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2022-10-21 01:11:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
-resource "null_resource" "env_test"{
-
-  provisioner "local-exec"{
+resource "null_resource" "env_test" {
+  
+  provisioner "local-exec" {
     command = "echo **** Env script test *******"
-
   }
 
-  provisioner "local-exec"{
+  provisioner "local-exec" {
 
 
     command = "printenv"
   }
 
-  provisioner "local-exec"{
+  provisioner "local-exec" {
 
     command = "echo **** Env script test *******"
   }
-
 }
