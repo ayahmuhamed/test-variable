15a8d10b2a912a7c347c13348e36b7305ec2540e
ayahmuhamed <88331884+ayahmuhamed@users.noreply.github.com>
2022-10-21 00:49:35 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resouce "null_resource" "env_test" { 
+
+  provisioner "local-exec" { 
+    command "echo **** Env script test *******"
+  
+  }
+   
+  provisioner "local-exec" {
+  
+   command = "printenv"
+  }
+     
+  provisioner "local-exec" { 
+    command "echo **** Env script test *******"
+  }  
+
+}
